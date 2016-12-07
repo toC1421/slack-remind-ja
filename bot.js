fc963a859cc4bf50f3b2ac02dabf2fbc7c961ec5
toC1421 <s1421030@gmail.com>
2016-12-07 19:39:07 +0000

    add token process

--- bot.js
+++ bot.js
@@ -1,26 +1,30 @@
 var botkit = require('botkit');
 
+var token = process.env.BOTKIT_SLACK_VERIFY_TOKEN;
 var controller = botkit.slackbot({
-  debug: false,
-  json_file_store: './simple_storage/'
+  debug: false
 }).configureSlackApp({
   clientId: process.env.BOTKIT_SLACK_CLIENT_ID,
   clientSecret: process.env.BOTKIT_SLACK_CLIENT_SECRET,
   scopes: ['commands']
-});
-
-controller.setupWebserver(process.env.PORT, function(err, webserver) {
+  }
+);
+controller.setupWebserver(process.env.PORT,function(err,webserver){
   controller.createWebhookEndpoints(controller.webserver);
   controller.createOauthEndpoints(controller.webserver, function(err, req, res) {
     if (err) {
       res.status(500).send('Error: ' + JSON.stringify(err));
     } else {
-      res.send('Success');
+      res.send('Success!');
     }
   });
 });
 
 controller.on('slash_command', function(bot, message) {
+  if (message.token !== token) {
+    return bot.res.send(401, 'Unauthorized');
+  }
+
   switch (message.command) {
   case '/remindja':
     var choices = message.text.split(',');
